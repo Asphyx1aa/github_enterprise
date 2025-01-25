e1c4546e5958a1db4542d9d16a5e51878206735e
Timur <tim.vlasov96@gmail.com>
2025-01-25 07:42:57 +0300

    created test

--- src/test/java/GithubEnterpriseTest.java
+++ src/test/java/GithubEnterpriseTest.java
@@ -1,6 +1,13 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class GithubEnterpriseTest {
     @BeforeAll
@@ -11,6 +18,9 @@ public class GithubEnterpriseTest {
 
     @Test
     void GithubEnterprisePageTest() {
-        
+        open("https://github.com/");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        $("title").shouldHave(attribute("text", "The AI Powered Developer Platform. · GitHub"));
     }
 }
